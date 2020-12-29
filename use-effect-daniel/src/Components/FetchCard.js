3db3f514841e6ad0a11dfe8ab09976a5be384a98
Daniel Vazquez <dvazquez@ganaenergia.com>
2020-12-29 17:03:49 +0100

    alimentado por el fetch

--- use-effect-daniel/src/Components/FetchCard.js
+++ use-effect-daniel/src/Components/FetchCard.js
@@ -1,15 +1,50 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import getUser from '../helpers/getUser';
+import getPosts from '../helpers/getPosts';
 
 const FetchCard = () => {
+    
+    // const initialUser = {
+    //     name: "Daniel",
+    //     email: "daniel.vazquezguerra@gmaill.com",
 
-    const initialUser = {
-        name: "Daniel",
-        email: "daniel.vazquezguerra@gmaill.com",
+    // }
+
+    // const initialPosts = [
+    //     {id: 1, title: "Post 1"},
+    //     {id: 2, title: "Post 2"}
+    // ];
+    const [user, setUser] = useState({});
+    const [posts, setPosts] = useState([]);
+    
+
+    const updateUser = () => {
+
+        getUser()
+        .then((newUser)=> {
+            setUser(newUser);
+        })
 
     }
+
+    const updatePosts = () => {
+
+        getPosts()
+        .then((newPosts)=>{
+            setPosts(newPosts);
+        })
+
+    }
+
+    useEffect(() => {
+      updateUser();
     
-    const [user, setUser] = useState(initialUser)
+    }, [])
+
+    useEffect(() => {
+      updatePosts();
+    }, [user])
+    
 
     
     return (
@@ -18,6 +53,22 @@ const FetchCard = () => {
 
             <h1>{user.name}</h1>
             <h2>{user.email}</h2>
+
+            <h2>Posts - user: {user.id}</h2>
+
+                <ul>
+                    {
+                    
+                        posts.map(post =>(
+
+                            <li key={post.id}>{post.name}</li>
+                            
+                        ))
+                    
+                    }
+                </ul>
+
+                <button onClick={updateUser}>Otro usuario</button>
             
         </div>
     )
---
+++ use-effect-daniel/src/helpers/getPosts.js
@@ -0,0 +1,11 @@
+const getPosts = async (userId) => {
+
+    const url = `https://jsonplaceholder.typicode.com/comments?postId=1`;
+    const res = await fetch(url);
+    const posts = await res.json();
+
+    return posts;
+
+}
+
+export default getPosts;
